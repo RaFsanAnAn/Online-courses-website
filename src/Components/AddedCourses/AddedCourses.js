22294fc07b511c5794812b1c096d50c31391e893
Rafsan Anan <anan.dhaka@gmail.com>
2020-08-27 21:59:39 +0600

    fixed a minor bug

--- src/Components/AddedCourses/AddedCourses.js
+++ src/Components/AddedCourses/AddedCourses.js
@@ -2,11 +2,12 @@ import React from 'react';
 import "./AddedCourses.css"
 
 const AddedCourses = (props) => {
-    const {name, instructor} = props.course;
+    const {name, instructor, price} = props.course;
     return (
         <div className="bg-dark added-course">
             <h3>{name}</h3>
             <h5>Instructed by : {instructor}</h5>
+            <h5>Price : {price} </h5>
         </div>
     );
 };
